e3af4147a5f6dea5c23828aab8c90c49d55b889b
QSDE2607 <manhquangdata@gmail.com>
2024-06-04 18:08:35 +0700

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+ENDPOINT: asm2-db.c5kk8g24wmem.ap-southeast-2.rds.amazonaws.com
+USERNAME: admin
+Password: Khongbiet12
+DB Name: ASM2DB
---
+++ SQLQuery1.sql
@@ -0,0 +1,42 @@
+﻿//Truy van 1:
+SELECT
+    a.Category,
+    SUM(r.Sentiment_Subjectivity) AS SUM_Sentiment_Subjectivity,
+    SUM(r.Sentiment_Polarity) AS SUM_Sentiment_Polarity
+FROM
+    dbo.review r
+        JOIN dbo.app a ON r.App = a.App
+GROUP BY
+    a.Category;
+
+//Truy van 2:
+SELECT
+    a.Category,
+    SUM(CASE WHEN r.Sentiment = 'Positive'  THEN 1 ELSE 0 END) AS Positive_Count,
+    SUM(CASE WHEN r.Sentiment = 'Negative'  THEN 1 ELSE 0 END) AS Negative_Count,
+    SUM(CASE WHEN r.Sentiment = 'Neutral'  THEN 1 ELSE 0 END) AS Neutral_Count
+FROM
+    dbo.review r
+        JOIN dbo.app a ON r.App = a.App
+GROUP BY
+    a.Category;
+
+//Truy vấn 3:
+
+ALTER TABLE app
+ALTER COLUMN Price float
+ALTER COLUMN Rating float
+
+SET STATISTICS TIME ON;
+SELECT TOP 1
+    a.Category,
+    SUM(LEN(r.Translated_Review) - LEN(REPLACE(r.Translated_Review, ' ', '')) + 1) AS WordCount,
+    AVG(a.Rating) AS Avg_Rating,
+    AVG(a.Price) AS Avg_Price
+FROM
+    review AS r
+        INNER JOIN app AS a ON r.app = a.app
+GROUP BY
+    a.Category
+ORDER BY WordCount DESC
+ SET STATISTICS TIME OFF;
